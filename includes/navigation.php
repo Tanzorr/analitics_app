b4177f061facb795b0892e7086f72e8b1a42b8e6
tanzorr <alexx1984@ukr.net>
2020-10-28 10:23:00 +0200

    litle fix

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-sm bg-light navbar-light">
     <ul class="navbar-nav">
         <li class="nav-item active">
-            <a class="nav-link" href="#">Active</a>
+            <a class="nav-link" href="/analitics_app/index.php">Home</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
